d38bc6421a5084476e91106e31fed9be8e482c78
KisGabo <kisgabo94@gmail.com>
2016-11-21 19:25:42 +0100

    Fix bug of stucked messages in view globals

--- app/Http/Middleware/MessagesToView.js
+++ app/Http/Middleware/MessagesToView.js
@@ -2,23 +2,19 @@
 
 const View = use('Adonis/Src/View')
 
-let _messages = []
-
 class MessagesToView {
 
   * handle (req, resp, next) {
     if (req.old('messages')) {
-      _messages = req.old('messages')
+      req.messages = req.old('messages')
+    }
+    else {
+      req.messages = []
     }
 
-    View.global('messages', _messages)
+    View.global('messages', req.messages)
 
-    resp.addMessage = this.addMessage;
     yield next
-  }
-
-  addMessage(type, message) {
-    _messages.push({ type, message })
   }
 
 }
