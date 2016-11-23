ced0863cd9ffebe689bb713301c8b466915dd472
KisGabo <kisgabo94@gmail.com>
2016-11-23 11:15:28 +0100

    Add a custom helper method to Lucid models

--- bootstrap/events.js
+++ bootstrap/events.js
@@ -16,6 +16,33 @@
 |
 */
 const Event = use('Event')
+const ioc = require('adonis-fold').Ioc
 
 Event.when('Http.error.*', 'Http.handleError')
 Event.when('Http.start', 'Http.onStart')
+
+/**
+ * A little hack to have a new handy method on ALL Lucid model instances.
+ * 
+ * .relatedNotLoaded(..) does what the original .related(..) does,
+ * but won't add relations that are already loaded, to the eager loading queue.
+ */
+
+const _ = require('lodash')
+
+use('Lucid').prototype.relatedNotLoaded = function() {
+  const relations = _.isArray(arguments[0]) ? arguments[0] : _.toArray(arguments)
+
+  relations.filter(rel => {
+      const splitted = rel.split('.')
+      let data = this.relations
+      for (rel of splitted) {
+      data = data[rel]
+      if (!data) return true
+      }
+      return false
+  });
+
+  this.eagerLoad.with(relations)
+  return this
+}
