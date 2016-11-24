bd0897c17cdcd7d836cb307377500715b88dce35
KisGabo <kisgabo94@gmail.com>
2016-11-24 02:03:11 +0100

    Refactor

--- bootstrap/custom.js
+++ bootstrap/custom.js
@@ -57,4 +57,30 @@ module.exports.boot = function() {
   Lucid.prototype.formatDate = function(date) {
     return date
   }
+
+  /**
+   * A little hack to have a new handy method on ALL Lucid model instances.
+   * 
+   * .relatedNotLoaded(..) does what the original .related(..) does,
+   * but won't add relations that are already loaded, to the eager loading queue.
+   */
+
+  const _ = require('lodash')
+
+  Lucid.prototype.relatedNotLoaded = function() {
+    const relations = _.isArray(arguments[0]) ? arguments[0] : _.toArray(arguments)
+
+    relations.filter(rel => {
+        const splitted = rel.split('.')
+        let data = this.relations
+        for (rel of splitted) {
+        data = data[rel]
+        if (!data) return true
+        }
+        return false
+    });
+
+    this.eagerLoad.with(relations)
+    return this
+  }
 }
