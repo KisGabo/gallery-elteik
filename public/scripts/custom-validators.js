ae04d1c0611792e83af166715362b323939b0540
KisGabo <kisgabo94@gmail.com>
2016-12-01 02:07:57 +0100

    Validate dates with BS validator

---
+++ public/scripts/custom-validators.js
@@ -0,0 +1,23 @@
+/**
+ * Custom validator functions for Bootstrap validator.
+ */
+
+$.fn.validator.Constructor.DEFAULTS.custom = {
+  
+  datetime: function($el) {
+    if (!moment($el.val(), 'YYYY.MM.DD. HH:mm:ss', true).isValid()) {
+      return 'A dátum formátuma a következő legyen: YYYY.MM.DD. HH:mm:ss'
+    }
+  },
+
+  /*
+  requiredother: function($el) {console.log("val")
+    const $other = $el.data('requiredother')
+    const otherVal = $($other).val()
+    if (!otherVal && $el.val()) {
+      return 'Majd a megerősítést is töltsd ki.'
+    }
+  },
+  */
+
+}
